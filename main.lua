2a5140b8db2e5ad5c586835c71effec37495228a
Jesse Ruder <jesse@getexponent.com>
2019-07-16 21:08:46 -0700

    initial

---
+++ main.lua
@@ -0,0 +1,232 @@
+-- Render constants
+local RENDER_SCALE = 3
+
+-- Game constants
+local LEVEL_DATA = {{type = 'block', x = 0, y = 2, width = 1, height = 1}}
+for i=1,20 do
+  table.insert(LEVEL_DATA, {type = 'block', x = i, y = 8, width = 1, height = 1})
+end
+
+local BLOCK_SIZE = 16
+
+function TO_WORLD_UNITS(x)
+  -- 60 fps
+  -- https://web.archive.org/web/20130807122227/http://i276.photobucket.com/albums/kk21/jdaster64/smb_playerphysics.png
+  return (x / 0xFFFFF) * BLOCK_SIZE * 60
+end
+
+local MINIMUM_WALK_VELOCITY = TO_WORLD_UNITS(0x00130)
+local MAXIMUM_WALK_VECLOCITY = TO_WORLD_UNITS(0x01900)
+local WALK_ACCELERATION = TO_WORLD_UNITS(0x00098)
+local MAXIMUM_WALK_VELOCITY_UNDERWATER = TO_WORLD_UNITS(0x1100)
+local RUN_ACCELERATION = TO_WORLD_UNITS(0x000E4)
+local MAXIMUM_WALK_VELOCITY_LEVEL_ENTRY = TO_WORLD_UNITS(0x00D00)
+local RELEASE_DECELERATION = TO_WORLD_UNITS(0x000D0)
+local MAXIMUM_RUN_VELOCITY = TO_WORLD_UNITS(0x02900)
+local SKID_DECELERATION = TO_WORLD_UNITS(0x001A0)
+local SKID_TURNAROUND_SPEED = TO_WORLD_UNITS(0x00900)
+
+
+-- Game variables
+local player
+local platforms
+local gems
+
+-- Assets
+local playerImage
+local objectsImage
+local walkSounds
+local jumpSound
+local landSound
+local gemSound
+
+function resetGame()
+  -- Create platforms and game objects from the level data
+  platforms = {}
+  gems = {}
+
+  -- Create the player
+  player = {
+    x = 1,
+    y = 1,
+    vx = 0,
+    vy = 0,
+    width = BLOCK_SIZE,
+    height = BLOCK_SIZE,
+  }
+
+  for _, obj in ipairs(LEVEL_DATA) do
+    if obj.type == 'block' then
+      table.insert(platforms, {
+        x = obj.x * BLOCK_SIZE,
+        y = obj.y * BLOCK_SIZE,
+        width = obj.width * BLOCK_SIZE,
+        height = obj.height * BLOCK_SIZE
+      })
+    end
+  end
+end
+
+-- Initializes the game
+function love.load()
+  -- Load assets
+  playerImage = love.graphics.newImage('img/player.png')
+  objectsImage = love.graphics.newImage('img/objects.png')
+  playerImage:setFilter('nearest', 'nearest')
+  objectsImage:setFilter('nearest', 'nearest')
+  walkSounds = {
+    love.audio.newSource('sfx/walk1.wav', 'static'),
+    love.audio.newSource('sfx/walk2.wav', 'static')
+  }
+  jumpSound = love.audio.newSource('sfx/jump.wav', 'static')
+  landSound = love.audio.newSource('sfx/land.wav', 'static')
+  gemSound = love.audio.newSource('sfx/gem.wav', 'static')
+
+  resetGame()
+end
+
+function love.keypressed(key, scancode, isrepeat)
+  local moveX = (key == 'left' and -1 or 0) + (key == 'right' and 1 or 0)
+
+  if player.vx == 0 then
+    player.vx = moveX * MINIMUM_WALK_VELOCITY
+  end
+end
+
+-- Updates the game state
+function love.update(dt)
+  -- Accelerate downward (a la gravity)
+  player.vy = player.vy + 480 * dt
+
+  -- Apply the player's velocity to her position
+  player.x = player.x + player.vx * dt
+  player.y = player.y + player.vy * dt
+
+  -- Check for collisions with platforms
+  local wasGrounded = player.isGrounded
+  player.isGrounded = false
+  for _, platform in ipairs(platforms) do
+    local collisionDir = checkForCollision(player, platform)
+    if collisionDir == 'top' then
+      player.y = platform.y + platform.height
+      player.vy = math.max(0, player.vy)
+    elseif collisionDir == 'bottom' then
+      player.y = platform.y - player.height
+      player.vy = math.min(0, player.vy)
+      player.isGrounded = true
+      if not wasGrounded then
+        love.audio.play(landSound:clone())
+      end
+    elseif collisionDir == 'left' then
+      player.x = platform.x + platform.width
+      player.vx = math.max(0, player.vx)
+    elseif collisionDir == 'right' then
+      player.x = platform.x - player.width
+      player.vx = math.min(0, player.vx)
+    end
+  end
+
+  -- Check for gem collection
+  for _, gem in ipairs(gems) do
+    if not gem.isCollected and entitiesOverlapping(player, gem) then
+      gem.isCollected = true
+      love.audio.play(gemSound:clone())
+    end
+  end
+
+  -- Keep the player in bounds
+  if player.x < 0 then
+    player.x = 0
+  end
+  --[[elseif player.x > GAME_WIDTH - player.width then
+    player.x = GAME_WIDTH - player.width
+  end]]--
+  if player.y > love.graphics.getHeight() / RENDER_SCALE + 50 then
+    player.y = -10
+  end
+end
+
+-- Renders the game
+function love.draw()
+  -- Scale and crop the screen
+  --love.graphics.setScissor(0, 0, RENDER_SCALE * GAME_WIDTH, RENDER_SCALE * GAME_HEIGHT)
+  love.graphics.scale(RENDER_SCALE, RENDER_SCALE)
+  love.graphics.clear(251 / 255, 134 / 255, 199 / 255)
+  love.graphics.setColor(1, 1, 1, 1)
+
+  local screenWidth = love.graphics.getWidth()
+  local screenHeight = love.graphics.getHeight()
+
+  --[[
+  for x=0, screenWidth, BLOCK_SIZE do
+    love.graphics.line(x, 0, x, screenHeight)
+  end
+
+  for y=0, screenHeight, BLOCK_SIZE do
+    love.graphics.line(0, y, screenWidth, y)
+  end]]--
+
+  -- Draw the platforms
+  for _, platform in ipairs(platforms) do
+    drawSprite(objectsImage, 16, 16, 1, platform.x, platform.y)
+  end
+
+  -- Draw the gems
+  for _, gem in ipairs(gems) do
+    if not gem.isCollected then
+      drawSprite(objectsImage, 16, 16, 2, gem.x, gem.y)
+    end
+  end
+
+  -- Draw the player
+  local sprite
+  if player.isGrounded then
+    sprite = 1
+  -- When jumping
+  elseif player.vy > 0 then
+    sprite = 6
+  else
+    sprite = 5
+  end
+  drawSprite(playerImage, 16, 16, sprite, player.x, player.y, player.isFacingLeft)
+
+  --renderButtons()
+end
+
+-- Draws a sprite from a sprite sheet, spriteNum=1 is the upper-leftmost sprite
+function drawSprite(spriteSheetImage, spriteWidth, spriteHeight, sprite, x, y, flipHorizontal, flipVertical, rotation)
+  local width, height = spriteSheetImage:getDimensions()
+  local numColumns = math.floor(width / spriteWidth)
+  local col, row = (sprite - 1) % numColumns, math.floor((sprite - 1) / numColumns)
+  love.graphics.draw(spriteSheetImage,
+    love.graphics.newQuad(spriteWidth * col, spriteHeight * row, spriteWidth, spriteHeight, width, height),
+    x + spriteWidth / 2, y + spriteHeight / 2,
+    rotation or 0,
+    flipHorizontal and -1 or 1, flipVertical and -1 or 1,
+    spriteWidth / 2, spriteHeight / 2)
+end
+
+-- Determine whether two rectangles are overlapping
+function rectsOverlapping(x1, y1, w1, h1, x2, y2, w2, h2)
+  return x1 + w1 > x2 and x2 + w2 > x1 and y1 + h1 > y2 and y2 + h2 > y1
+end
+
+-- Returns true if two entities are overlapping, by checking their bounding boxes
+function entitiesOverlapping(a, b)
+  return rectsOverlapping(a.x, a.y, a.width, a.height, b.x, b.y, b.width, b.height)
+end
+
+-- Checks to see if two entities are colliding, and if so from which side. This is
+-- accomplished by checking the four quadrants of the axis-aligned bounding boxes
+function checkForCollision(a, b)
+  local indent = 3
+  if rectsOverlapping(a.x + indent, a.y + a.height / 2, a.width - 2 * indent, a.height / 2, b.x, b.y, b.width, b.height) then
+    return 'bottom'
+  elseif rectsOverlapping(a.x + indent, a.y, a.width - 2 * indent, a.height / 2, b.x, b.y, b.width, b.height) then
+    return 'top'
+  elseif rectsOverlapping(a.x, a.y + indent, a.width / 2, a.height - 2 * indent, b.x, b.y, b.width, b.height) then
+    return 'left'
+  elseif rectsOverlapping(a.x + a.width / 2, a.y + indent, a.width / 2, a.height - 2 * indent, b.x, b.y, b.width, b.height) then
+    return 'right'
+  end
+end
